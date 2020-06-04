f01d3676dad25fe9e54673752983cc691e02d759
thameemansarij <athameem35@gmail.com>
2020-06-04 12:20:53 +0530

    new

--- js/main-js.js
+++ js/main-js.js
@@ -40,7 +40,7 @@ var top =  $(".goto-top");
 if ( $('body').height() <= (    $(window).height() + $(window).scrollTop() + 200 )) {
 top.animate({"margin-left": "0px"},1500);
 } else {
-top.animate({"margin-left": "-100%"},1500);
+top.animate({"margin-left": "0px"},1500);
 }
 });
 
@@ -56,7 +56,7 @@ $("html, body").animate({scrollTop: 0}, 1000);
 });*/
 /*Active line*/
 /*Pause-slider*/
-/*$(window).load(function() {
+$(window).load(function() {
     $('.carousel').carousel('pause'); 
-});*/
+});
 /*Pause-slider*/
